b36828cddc727536b16f6a2b1c192e45c7f8df4e
nfikriyafi-rgb <nfikriyafi@gmail.com>
2026-02-04 21:25:42 +0700

    Clean up imports in index.js
    Removed dotenv and fs imports from index.js.
    
    Signed-off-by: nfikriyafi-rgb <nfikriyafi@gmail.com>

--- index.js
+++ index.js
@@ -1,10 +1,4 @@
-require("dotenv").config();
-const fs = require("fs");
-const {
-  Client,
-  GatewayIntentBits,
-  Partials
-} = require("discord.js");
+const { Client, GatewayIntentBits } = require("discord.js");
 
 const client = new Client({
   intents: [
